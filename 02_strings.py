62421d1c67a5b67776574ee6361c0be794b1f93f
sudogaan <sudoogaan@gmail.com>
2022-04-14 15:05:59 +0300

    uodate

---
+++ 02_strings.py
@@ -0,0 +1,8 @@
+message = "Meet me tonight."
+print(message)
+message2 = 'The clock strikes at midnight.'
+print(message2)
+movie_quote = """One of my favorite lines from Th Godfather is:
+"I'm going to make him an offer he can't refuse."
+Do you know who said this?"""
+print(movie_quote)
